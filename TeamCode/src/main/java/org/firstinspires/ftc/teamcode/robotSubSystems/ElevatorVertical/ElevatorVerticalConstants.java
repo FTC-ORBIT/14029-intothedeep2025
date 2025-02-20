8e6a2f5784f6e0e2c89a190a1d5431f6c475e5ff
Snoulest <pilkatan@gmail.com>
2025-02-20 07:36:31 +0200

    started working on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/ElevatorVertical/ElevatorVerticalConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/ElevatorVertical/ElevatorVerticalConstants.java
@@ -14,7 +14,7 @@ public class ElevatorVerticalConstants {
 
     public static int IntakePos = 155;
 
-    public static int SpecimenPos = 340;
+    public static int SpecimenPos = 280;
     public static int PutSpecimenPos = 1600;
     public static int DepletePos = 2300;
 
